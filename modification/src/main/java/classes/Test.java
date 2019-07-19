6c452d103a012bb26c681158efbfaddb7a4a30c9
duquej <johndduque@gmail.com>
2019-07-18 21:50:49 -0400

    connected back end and front end

--- modification/src/main/java/classes/Test.java
+++ modification/src/main/java/classes/Test.java
@@ -17,15 +17,29 @@ public class Test {
 		   Event test = new Event();
 		   test.setBeginDate(new Date(2019-10-03));
 		   test.setEndDate(new Date(2019-10-03));
-		   test.setName("bowling");
+		   test.setName("Terris Cook Out");
 		   
 		   User user = new User();
-		   user.setName("zod");
-		   user.setPhoneNumber("781");
+		   user.setName("Jorge");
+		   user.setPhoneNumber("781-555-555");
+		   user.setStatus(1);
+		   
+		   User user1 = new User();
+		   user1.setName("Terris");
+		   user1.setPhoneNumber("781-555-555");
+		   user1.setStatus(0);
+		   
+		   User user2 = new User();
+		   user2.setName("Reina");
+		   user2.setPhoneNumber("781-555-555");
+		   user2.setStatus(2);
 		   
 		   List<User> users = new ArrayList();
 		   List<Event> events = new ArrayList();
 		   users.add(user);
+		   users.add(user1);
+		   users.add(user2);
+		   
 		   events.add(test);
 		   
 		   
--- modification/src/main/webapp/js/functions.js
+++ modification/src/main/webapp/js/functions.js
@@ -1,7 +1,26 @@
 /**
  * 
  */
+var eventDetails;
+var event;
+var users = [];
 
+$(document).ready(function(){
+    var date_input=$('input[name="begindate"]'); 
+    var date_input2=$('input[name="enddate"]'); 
+
+    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+    var options={
+      format: 'yyyy/mm/dd',
+      container: container,
+      todayHighlight: false,
+      autoclose: true,
+    };
+    date_input.datepicker(options);
+    date_input2.datepicker(options);
+  })
+  
+  
 $( document ).ready(function() {
 	
 	//TODO: FIX LOADING DATES. 
@@ -26,6 +45,8 @@ function onLaunch(){
 }
 
 function loadEventData(){
+	
+	document.getElementById("queryDate").value = "2019-07-16";
 		
 	$.ajax({
 	    type:"GET",
@@ -33,14 +54,82 @@ function loadEventData(){
 	    data:"date="+document.getElementById("queryDate").value ,
 	    success: function(data){
 	        console.log(data);
+	        defaultLoad(data);
+	        event = data;
 	    }
 	});
 	
 	
 }
 
-function defaultLoad(){
+function defaultLoad(data){
+
 	
+	$.each(data, function(i, event ) {
+		
+
+		eventDetails = {name:event.name, 
+				beginDate: event.beginDate,
+				endDate: event.endDate}
+				
+		
+		$.each(event.users, function(inc, user){
+			
+			
+			
+			users[inc] = {name: user.name, 
+					status: user.status, 
+					phonenumber: user.phoneNumber}			
+			
+			
+		});
+		
+		
+		
+		  
+		});
+	
+	imageDisplayControl(users, event)
+	
+}
+
+function imageDisplayControl(data){
+	
+	for (var i =0; i < data.length; i++){
+		var name = data[i].name;
+		var phone = data[i].phonenumber
+		var status;
+		var imgsrc;
+		var eventName = eventDetails.name;
+		var beginDate = eventDetails.beginDate;
+		var endDate = eventDetails.endDate;
+		
+		if (data[i].status === 0 ) {
+			status = "Going";
+			imgsrc= "images/greenDot.png"
+		} else if (data[i].status === 1) {
+			status = "Not going";
+			imgsrc = "images/redDot.jpg";
+			
+		} else if (data[i].status === 2) {
+			status = "Maybe";
+			imgsrc = "images/yellowDot.png"
+		} else if (data[i].status === 3){
+			status = "Unknown";
+			imgsrc = "images/greyDot.png";
+		}
+		
+		eventname.innerText = "Event Name: "+ eventName;
+
+		
+		
+		$('#users tr:last').after('<tr><td align="left"> <img src="'+imgsrc+'" align="texttop" alt="IN" width="14" height="14" title="reserve" onclick="">'
+				+name+"</td> <td align='left'>"+status+"</td> <td align='left'>"+ phone+"</td></tr>");
+				
+	}
 	
 	
 }
+
+
+
